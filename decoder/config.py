c587431ab3873689f736d460b42034e92d62a1ae
Curtis <curtis@amplitude.com>
2014-05-16 19:41:15 -0700

    Remove unnecessary files

---
+++ decoder/config.py
@@ -0,0 +1,8 @@
+# config
+BROKER_URL='amqp://guest@localhost//'
+CELERY_RESULT_BACKEND='amqp://guest@localhost//'
+BASE_DIR = '/srv/crash'
+AWS_CACHE_DIR = '/tmp/s3cache'
+AWS_ACCESS_KEY_ID='AWS_ACCESS_KEY_ID'
+AWS_SECRET_ACCESS_KEY='AWS_SECRET_ACCESS_KEY'
+AWS_BUCKET = 'aws-s3-bucket'
--- server/run_server.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-source .env/bin/activate
-exec python server.py $@
-deactivate
